337450d156aa452c11030f55712aebd9de6a6bd7
alissonwr <alisson-wendel@hotmail.com>
2024-09-25 21:26:07 -0300

    Criando o pacote package_name_Alisson

--- image-processing-package/setup.py
+++ image-processing-package/setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing",
+    name="image_processing_alisson",
     version="0.0.1",
     author="Alisson",
     description="Image Processing Package using Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/alissonwr/Desafios_Ntt_Data_Dio/tree/1bfc7e799f6ae9519bec3bf8c668cda0931e30ec/image-processing-package",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.5',
 )
